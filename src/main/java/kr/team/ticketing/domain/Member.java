6512ce8e8f9b65297aa835e2c02a3a6341535bda
Rebwon <msolo021015@naver.com>
2020-02-04 13:37:54 +0900

    docs: 주석제거

--- src/main/java/kr/team/ticketing/domain/Member.java
+++ src/main/java/kr/team/ticketing/domain/Member.java
@@ -1,7 +1,4 @@
 package kr.team.ticketing.domain;
 
-/**
- * Created by kimdonghyun on 03/02/2020.
- */
 public class Member {
 }
--- src/test/java/kr/team/ticketing/MemberSaveTest.java
+++ src/test/java/kr/team/ticketing/MemberSaveTest.java
@@ -2,9 +2,6 @@ package kr.team.ticketing;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-/**
- * Created by kimdonghyun on 04/02/2020.
- */
 @SpringBootTest
 public class MemberSaveTest {
 }
