b87046e4a9e77963584b1c11b3ce3436dfacf602
Rebwon <msolo021015@naver.com>
2020-02-05 17:48:46 +0900

    Revert "feat: 기본적인 도메인 설계 완료."
    This reverts commit b816a741db2a4be5d4e0597f3e2611790d6eac86.

--- src/main/java/kr/team/ticketing/domain/member/Member.java
+++ src/main/java/kr/team/ticketing/domain/member/Member.java
@@ -1,17 +1,4 @@
 package kr.team.ticketing.domain.member;
 
-import lombok.Getter;
-
-import javax.persistence.*;
-
-@Getter
-@Table(name = "MEMBERS")
-@Entity
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "MEMBER_ID")
-    private Long id;
-
-    private String name;
 }
