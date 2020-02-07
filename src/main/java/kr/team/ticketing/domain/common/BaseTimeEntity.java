810fd14786d7c534295801265de9abab150d0af3
Rebwon <msolo021015@naver.com>
2020-02-07 11:37:52 +0900

    docs: BaseTimeEntity 수정 및 Config 추가.

--- src/main/java/kr/team/ticketing/domain/common/BaseTimeEntity.java
+++ src/main/java/kr/team/ticketing/domain/common/BaseTimeEntity.java
@@ -3,11 +3,16 @@ package kr.team.ticketing.domain.common;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
-public class BaseTimeEntity {
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
 
     @CreatedDate
     private LocalDateTime createdDate;
