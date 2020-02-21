7c1e7a931fc1999003fb232375db3350e6a66fbb
Rebwon <msolo021015@naver.com>
2020-02-21 17:46:48 +0900

    feat: 테스트 환경과 로컬 환경의 DB 설정 변경 및 테스트 진행.

--- src/test/java/kr/team/ticketing/common/ControllerTests.java
+++ src/test/java/kr/team/ticketing/common/ControllerTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
+@ActiveProfiles("test")
 @Import(RestDocsConfig.class)
 @Ignore
 public class ControllerTests {
